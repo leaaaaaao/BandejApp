8f1150c8655125f31da3c16587d14d7ffd909ffe
unknown <raphafm.rf@gmail.com>
2024-04-09 19:03:08 -0300

    implementação da header sticky, ocultando após certa quantidade de scroll

--- src/Components/Cabecalho/index.tsx
+++ src/Components/Cabecalho/index.tsx
@@ -1,5 +1,5 @@
 import { NotificationContext } from "../../Contexts/PendingNotificationContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { abrirSideBar } from "../../Functions/SideBar/abrirEfechar";
 
 import { PlaceHolderCabecalho, CabecaDiv, PageTitle, DivAjustes, IconeAjustes,
@@ -47,12 +47,43 @@ const clique = (controle?: Function, setar?: Function) => {
     }
 }
 
+const useScrollDirection = (nome: String) => {
+    const stickyLimit = nome === 'Cardápio' ? 400: 20;
+    const stickySensitivity = 5;
+    const [scrollDirection, setScrollDirection] = useState("");
+
+    useEffect(() => {
+        let lastScrollY = window.scrollY;
+
+        const updateScrollDirection = () => {
+            const scrollY = window.scrollY;
+            const direction = scrollY > lastScrollY ? "down" : "up";
+            if (direction !== scrollDirection && (
+                    (scrollY - lastScrollY > stickySensitivity && scrollY > stickyLimit) || 
+                    (scrollY - lastScrollY < -stickySensitivity && scrollY < stickyLimit)
+                )) {
+                setScrollDirection(direction);
+            }
+            lastScrollY = scrollY > 0 ? scrollY : 0;
+        };
+        window.addEventListener("scroll", updateScrollDirection); // add event listener
+        return () => {
+            window.removeEventListener("scroll", updateScrollDirection); // clean up
+        }
+    }, [scrollDirection]);
+
+    return scrollDirection;
+};
+
 export default function Cabecalho({nome, setOpcoes}: Nome) {
     const { pendingNotification } = useContext(NotificationContext);
+    const scrollDirection = useScrollDirection(nome);
 
     return (
-        <PlaceHolderCabecalho style={window.innerWidth/window.innerHeight <= 1 ? {height: 'calc(8vh + 2.25vh)'} : {height: 'unset', width: '100%'}}
-            // style={{height: (window.innerWidth/window.innerHeight) <= 1 ? 'calc(8vh + 2.25vh)' : 'unset'}}
+        <PlaceHolderCabecalho 
+            style={window.innerWidth/window.innerHeight <= 1 ? 
+                {height: 'calc(8vh + 2.25vh)'} : 
+                {height: 'unset', width: '100%', position: 'sticky', zIndex: 99, transition: '500ms', left: 0, top: scrollDirection === "down" ? '-12vw' : 0}}
         >
             <SideBar/>
     
